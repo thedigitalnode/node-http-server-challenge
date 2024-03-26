2ebd81daa8f76c677fec01a04eb3f90169433f88
Will Dallmann <storied.exit0s@icloud.com>
2024-03-26 09:09:00 -1000

    Updated to be less than 10 lines of code

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
-const http = require('node:http')
-const HOST = 'localhost'
+const http = require('node:http');
+const HOST = 'localhost';
 const PORT = 3000;
 
 const server = http.createServer((request, response) => {
-  let url = request.url.split('/');
-  if (url[1] === '' ) {
-    if (request.method === 'GET') {
-      response.end("Hello, Galvanize!");
-    }
-  }
-  response.statusCode = 404;
-  response.end();
-});
-
-server.listen(PORT, HOST, () => {
+  response.end("Hello, Galvanize!");
   console.log(`Server running at ${HOST}:${PORT}`);
 });
+server.listen(PORT, HOST, () => { });
